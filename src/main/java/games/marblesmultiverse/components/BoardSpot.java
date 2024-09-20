3c1b43aacea24e44a7294715a20c0b9463298dae
joshua kritz <joshuakritz2@gmail.com>
2024-09-20 10:13:45 +0100

    update on required overrides

--- src/main/java/games/marblesmultiverse/components/BoardSpot.java
+++ src/main/java/games/marblesmultiverse/components/BoardSpot.java
@@ -1,6 +1,10 @@
 package games.marblesmultiverse.components;
 
+import com.univocity.parsers.annotations.Copy;
 import core.components.BoardNode;
+import scala.collection.parallel.mutable.UnrolledParArrayCombiner;
+
+import java.util.Objects;
 
 public class BoardSpot extends BoardNode {
 
@@ -26,6 +30,15 @@ public class BoardSpot extends BoardNode {
         occupant = null;
     }
 
+    public BoardSpot(int x, int y, SpotType type, int ID) {
+        super(-1, "create name with X and Y", ID);
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        occupant = null;
+    }
+
+
 
     // dunno if it is going to be necessary but just in case
     public boolean AddMarble(Marble newOccupant){
@@ -48,5 +61,25 @@ public class BoardSpot extends BoardNode {
             occupant = null;
             return true;
         }
+    }
+
+    @Override
+    public BoardSpot copy() {
+        BoardSpot = new BoardSpot(); // add stuff
+        return super.copy();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        BoardSpot boardSpot = (BoardSpot) o;
+        return x == boardSpot.x && y == boardSpot.y && type == boardSpot.type && Objects.equals(occupant, boardSpot.occupant);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), type, x, y, occupant);
     }
 }
--- src/main/java/games/marblesmultiverse/components/Marble.java
+++ src/main/java/games/marblesmultiverse/components/Marble.java
@@ -1,6 +1,9 @@
 package games.marblesmultiverse.components;
 
+import core.components.Component;
 import core.components.Token;
+
+import java.util.Objects;
 
 public class Marble extends Token {
 
@@ -20,4 +23,23 @@ public class Marble extends Token {
     }
 
     public MarbleColor getColor() {return  color;};
+
+    @Override
+    public Token copy(int playerId) {
+        return super.copy(playerId);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Marble marble = (Marble) o;
+        return color == marble.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), color);
+    }
 }
