52087bc5d67196bcbee9e25042904965a7d26694
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2024-10-10 12:04:21 +0100

    Update MMCardView.java

--- src/main/java/games/marblesmultiverse/gui/MMCardView.java
+++ src/main/java/games/marblesmultiverse/gui/MMCardView.java
@@ -14,24 +14,25 @@ public class MMCardView extends ComponentView {
 
     public MMCardView(Card c) {
         super(null, cardWidth, cardHeight);
-        this.setLayout(new FlowLayout());
-        JTextArea desc = new JTextArea(c.description);
+        this.setLayout(null);
+        JTextArea desc = new JTextArea(c.type + "\n\n" + c.description);
         desc.setLineWrap(true);
         desc.setWrapStyleWord(true);
         desc.setFont(desc.getFont().deriveFont(13f));
+        desc.setEditable(false);
+        desc.setFocusable(false);
+        desc.setBounds(10,10,cardWidth-20,cardHeight-20);
         add(desc);
-        //super(c);
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        int fontSize = g.getFont().getSize();
-        Rectangle rect = new Rectangle(5, 5, width-5, height-5);
-//        g.drawString("Lorem ipsum dolor sit amet. Sed quia quia nam assumenda quasi ea enim facilis ut sapiente nisi et nulla beatae quo sequi quae ex praesentium animi. Et vitae neque ea consequatur odio et voluptate exercitationem est nihil quos.", 5, 5);
+    protected void paintComponent(Graphics gg) {
+        Graphics2D g = (Graphics2D) gg;
+        g.setColor(Color.white);
+        g.fillRect(5, 5, cardWidth-5, cardHeight-5);
+        g.setColor(Color.black);
         g.drawRect(5, 5, cardWidth-5, cardHeight-5);
 //        CardView.drawCard(g2d, null, rect);
-//        g.drawString("Lorem ipsum dolor sit amet. Sed quia quia nam assumenda quasi ea enim facilis ut sapiente nisi et ", 2, (int) (20 + fontSize * 1.5));
     }
 
 }
