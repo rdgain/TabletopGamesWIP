d64a47003b363f11a4c1a43db186d575d28cfad2
Joshua Kritz <joshuakritz2@gmail.com>
2025-02-12 16:10:43 +0000

    Added the pushed marbles metric at end game

--- src/main/java/games/marblesmultiverse/metrics/MMMetrics.java
+++ src/main/java/games/marblesmultiverse/metrics/MMMetrics.java
@@ -38,4 +38,30 @@ public class MMMetrics implements IMetricsCollection {
             return columns;
         }
     }
+
+    public static class MarbelsPushedOut extends AbstractMetric {
+
+        @Override
+        protected boolean _run(MetricsGameListener listener, Event e, Map<String, Object> records) {
+            MMGameState state= (MMGameState) e.state;
+            for (int i=0; i< state.getNPlayers() ;i++){
+                records.put("Player"+ i+ "Pushed Marbles",state.getPlayerMarblesPushedOut().get(i).size());
+            }
+            return true;
+        }
+
+        @Override
+        public Set<IGameEvent> getDefaultEventTypes() {
+            return Collections.singleton(Event.GameEvent.GAME_OVER);
+        }
+
+        @Override
+        public Map<String, Class<?>> getColumns(int nPlayersPerGame, Set<String> playerNames) {
+            Map<String, Class<?>> columns = new HashMap<>();
+            for (int i=0; i< nPlayersPerGame ;i++){
+                columns.put("Player"+ i+ "Pusehd Marbles", Integer.class);
+            }
+            return columns;
+        }
+    }
 }
